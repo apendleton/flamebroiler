c4790929794fdb18dfb334619fde3fd5d0f025ba
Andrew Pendleton <apendleton@sunlightfoundation.com>
2012-08-24 14:06:14 -0400

    Class-ify the Python trie.

---
+++ flamebroiler.py
@@ -0,0 +1,22 @@
+import cffi, os, platform
+libtype = "dylib" if platform.uname()[0] == "Darwin" else "so"
+
+trie_ffi = cffi.FFI()
+trie_ffi.cdef("void* create_trie();")
+trie_ffi.cdef("void free_trie(void *trie);")
+trie_ffi.cdef("void trie_insert(void* trie, wchar_t *key, int key_length, wchar_t *value);")
+trie_ffi.cdef("void print_dot(void* trie);")
+libtrie = trie_ffi.dlopen(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libtrie.%s" % libtype))
+libtrieprint = trie_ffi.dlopen(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libtrieprint.%s" % libtype))
+
+class Trie(object):
+    def __init__(self):
+        self._trie = libtrie.create_trie()
+
+    def __setitem__(self, key, value):
+        ukey = unicode(key)
+        uval = unicode(value)
+        libtrie.trie_insert(self._trie, ukey, len(ukey), uval)
+
+    def print_dot(self):
+        libtrieprint.print_dot(self._trie)
--- trie_test.py
+++ trie_test.py
@@ -1,18 +1,7 @@
-import cffi, os, platform
-libtype = "dylib" if platform.uname()[0] == "Darwin" else "so"
+from flamebroiler import Trie
 
-ffi = cffi.FFI()
-ffi.cdef("void* create_trie();")
-ffi.cdef("void free_trie(void *trie);")
-ffi.cdef("void trie_insert(void* trie, wchar_t *key, int key_length, wchar_t *value);")
-ffi.cdef("void print_dot(void* trie);")
-libtrie = ffi.dlopen(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libtrie.%s" % libtype))
-libtrieprint = ffi.dlopen(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libtrieprint.%s" % libtype))
-
-trie = libtrie.create_trie()
+trie = Trie()
 for fruit in ["apple", "peach", "pear", "mango", "lemon", "lime", "applesauce", "pineapple", "kiwi", "passionfruit"]:
-    uf = unicode(fruit)
-    libtrie.trie_insert(trie, uf, len(uf), uf)
+    trie[fruit] = fruit
 
-libtrieprint.print_dot(trie)
-libtrie.free_trie(trie)
+trie.print_dot()
