6fa6b4a8f9647dfd9625d9e4c5c56c3e8d82af69
Andrew Pendleton <apendleton@sunlightfoundation.com>
2012-08-24 13:52:26 -0400

    Experimental platform agnosticism.

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
+UNAME := $(shell uname)
+
+ifeq ($(UNAME), Linux)
+LIBTYPE=so
+endif
+ifeq ($(UNAME), Darwin)
+LIBTYPE=dylib
+endif
+
 all: clean build
 
 clean:
 	rm -f *.dylib *.gch trie_test
 
 build:
-	gcc -shared -fPIC -o libtrie.dylib rbt.h trie.h rbt.c trie.c
-	gcc -shared -fPIC -o libtrieprint.dylib rbt.h trie.h trieprint.h trieprint.c
+	gcc -shared -fPIC -o libtrie.$(LIBTYPE) rbt.h trie.h rbt.c trie.c
+	gcc -shared -fPIC -o libtrieprint.$(LIBTYPE) rbt.h trie.h trieprint.h trieprint.c
 
 test:
 	gcc -L. -ltrie -ltrieprint -o trie_test rbt.h trie.h trieprint.h trie_test.c
--- trie_test.py
+++ trie_test.py
@@ -1,12 +1,13 @@
-import cffi, os
+import cffi, os, platform
+libtype = "dylib" if platform.uname()[0] == "Darwin" else "so"
 
 ffi = cffi.FFI()
 ffi.cdef("void* create_trie();")
 ffi.cdef("void free_trie(void *trie);")
 ffi.cdef("void trie_insert(void* trie, wchar_t *key, int key_length, wchar_t *value);")
 ffi.cdef("void print_dot(void* trie);")
-libtrie = ffi.dlopen(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libtrie.dylib"))
-libtrieprint = ffi.dlopen(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libtrieprint.dylib"))
+libtrie = ffi.dlopen(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libtrie.%s" % libtype))
+libtrieprint = ffi.dlopen(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libtrieprint.%s" % libtype))
 
 trie = libtrie.create_trie()
 for fruit in ["apple", "peach", "pear", "mango", "lemon", "lime", "applesauce", "pineapple", "kiwi", "passionfruit"]:
