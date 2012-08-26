915423257791834d96bd19c909171c630d69d81d
Andrew Pendleton <abpend@gmail.com>
2012-08-26 15:51:44 -0400

    Add an outcome comparison between two search implementations.

---
+++ test/py/words_cmp.py
@@ -0,0 +1,27 @@
+from flamebroiler import Trie
+import resource
+import timeit
+
+def cmp_words():
+    letters = [unichr(i) for i in range(97,123)]
+    for first in letters:
+        for second in letters:
+            print first + second
+            d_match = set([words_d[k] for k in words_d.keys() if k.startswith(first + second)])
+            t_match = set(words_t.suffixes(first + second))
+            if t_match == d_match:
+                print 'yes'
+            else:
+                print t_match
+                print d_match
+
+print resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+words_t = Trie()
+words_d = {}
+for w in open("/usr/share/dict/words"):
+    word = unicode(w.strip().lower())
+    print word
+    words_t[word] = word
+    words_d[word] = word
+
+cmp_words()
